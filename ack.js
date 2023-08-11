198cf41ca13bd24c6f37e503731c914605994cb4
Tyler Eastman <tyler@lightshipworks.com>
2023-08-11 16:12:31 -0700

    Added a field for specifying a message ID property to use.  Fixed persistence bugs.

--- ack.js
+++ ack.js
@@ -13,28 +13,30 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
-const timers = require('timers');
-const fs = require('fs');
-const { join } = require('path')
-const FOLDER_NAME = "ack_msg_cache";
 
 module.exports = function (RED) {
     "use strict";
 
-    // Clean up any orphaned folders and files.  This can happen if the flow file is modified while the server is down.
+    const timers = require('timers');
+    const fs = require('fs-extra');
+    const path = require('path');
+
+    var FOLDER_NAME = "ack_msg_cache";
+    if (RED.settings.fileWorkingDirectory) {
+        FOLDER_NAME = path.join(RED.settings.fileWorkingDirectory, FOLDER_NAME);
+    }
+
+    // Clean up any orphaned folders and files.  Happens when ack-start nodes are deleted.
     RED.events.on("flows:started", function (event) {
-        // Get a list of ack-start nodes.
+        // Get list of ack-start nodes.
         const existingNodes = [];
         RED.nodes.eachNode(function (node) {
-            if (node.type === "ack-start")
+            if (node.type === "ack-start") {
                 existingNodes.push(node.id.replace(".", ""));
+            }
         });
 
-        if (!fs.existsSync(FOLDER_NAME)){
-            fs.mkdirSync(FOLDER_NAME, { recursive: true });
-        }
-
-        // Get list of ack_msg_cache folders
+        // Get list of ack_msg_cache node folders
         var directories = fs.readdirSync(FOLDER_NAME, { withFileTypes: true })
             .filter(dirent => dirent.isDirectory())
             .map(dirent => dirent.name);
@@ -42,10 +44,7 @@ module.exports = function (RED) {
         // Delete any folders that don't have a corresponding node.
         directories.forEach(directoryName => {
             if (existingNodes.indexOf(directoryName) < 0) {
-                fs.rmdir(join(FOLDER_NAME, directoryName), { recursive: true }, err => {
-                    if (err)
-                        throw err;
-                });
+                fs.rm(path.join(FOLDER_NAME, directoryName), { recursive: true });
             }
         });
     });
@@ -58,78 +57,108 @@ module.exports = function (RED) {
         node.ttlType = n.ttlType;
         node.units = n.units;
         node.unitsType = n.unitsType;
+        node.key = n.key;
+        node.keyType = n.keyType;
         node.timers = {};
         node.links = n.links;
         node.persist = n.persist;
 
-        const NODE_FOLDER_NAME = join(FOLDER_NAME, n.id.replace(".", ""));
+        const NODE_FOLDER_NAME = path.join(FOLDER_NAME, n.id.replace(".", ""));
 
+        // If the node is set to persistent, ensure that the persistence folder exists.
+        // Otherwise remove it if it does exist.
         if (node.persist) {
-            // Setup ack_msg_cache folder for this node.
-            if (!fs.existsSync(FOLDER_NAME))
-                fs.mkdirSync(FOLDER_NAME);
-
-            if (!fs.existsSync(NODE_FOLDER_NAME))
-                fs.mkdirSync(NODE_FOLDER_NAME);
-
-            // Read in all cached messages.
-            fs.readdirSync(NODE_FOLDER_NAME, { withFileTypes: true }).forEach(dirent => {
-                var raw = fs.readFileSync(join(NODE_FOLDER_NAME, dirent.name));
-                node.receive(JSON.parse(raw));
-            });
+            fs.ensureDirSync(NODE_FOLDER_NAME);
         }
-        else {
-            // If this is a non-persistent node delete the folder if it exists.
-            if (fs.existsSync(NODE_FOLDER_NAME)) {
-                fs.rmdir(NODE_FOLDER_NAME, { recursive: true }, err => {
-                    if (err)
-                        throw err;
-                });
-            }
+        else if (fs.existsSync(NODE_FOLDER_NAME)) {
+            fs.rm(NODE_FOLDER_NAME, { recursive: true });
         }
 
-        function addCacheMsg(msg, tod) {
-            if (!node.persist)
+        // Stores a msg to the filesystem.
+        function addPersistentMsg(msg, tod) {
+
+            // Exit if this isn't a persistent node.
+            if (!node.persist) {
                 return;
+            }
 
             msg._ack_cached_tod = tod;
-            fs.writeFile(join(NODE_FOLDER_NAME, msg._msgid.replace(".", "")), JSON.stringify(msg), err => {
-                if (err) throw err;
+
+            // Get the property value to use as the ID.
+            var key = RED.util.evaluateNodeProperty(node.key, node.keyType, node, msg);
+            var id = (msg.hasOwnProperty(key)) ? msg[key] : msg._msgid;
+            if(id.indexOf("/") > -1 || id.indexOf("\\") > -1) {
+                node.error('The value of the defined ID Property may not contain a "/" or "\\" character.');
+                return;
+            }
+
+            fs.writeFile(path.join(NODE_FOLDER_NAME, id), JSON.stringify(msg), err => {
+                if(err){
+                    node.error(`Failed to write persistent msg to file: ${path.join(NODE_FOLDER_NAME, id)}`);
+                }
             });
         }
 
-        function removeCacheMsg(id) {
-            if (!node.persist)
+        // Removes a msg from the filesystem.
+        function removePersistentMsg(id) {
+            if (!node.persist) {
                 return;
+            }
 
-            fs.unlink(join(NODE_FOLDER_NAME, id), err => {});
+            fs.rm(path.join(NODE_FOLDER_NAME, id), err => {
+                // Do nothing, the file may not exist.
+                // Could have been removed by another ack-clear node.
+            });
         }
 
+        // Callback for when an ack-clear is called.
         function handler(msg) {
-            var id = msg._msgid.replace(".", "");
-            removeCacheMsg(id);
+
+            // Get the property value to use as the ID.
+            var key = RED.util.evaluateNodeProperty(node.key, node.keyType, node, msg);
+            var id = (msg.hasOwnProperty(key)) ? msg[key] : msg._msgid;
+            if(id.indexOf("/") > -1 || id.indexOf("\\") > -1) {
+                node.error('The value of the defined ID Property may not contain a "/" or "\\" character.');
+                return;
+            }
+
+            removePersistentMsg(id);
+
+            // Clear the timer.
             if (node.timers[id]) {
                 timers.clearTimeout(node.timers[id].timer);
                 node.timers[id].done();
+                delete node.timers[id];
             }
         };
 
+        // Link the communication for ack-clear to the ack-start.
         node.links.forEach((link) => {
             RED.events.on("node:" + link, handler);
         });
 
-        this.on("input", function (msg, send, done) {
+        node.on("input", function (msg, send, done) {
             // If this node is installed in Node-RED 0.x provide fallbacks.
             send = send || function () { node.send.apply(node, arguments) }
             done = done || function (err) { if (err) { node.error(err, msg); } }
 
-            var id = msg._msgid.replace(".", "");
             var ttl = RED.util.evaluateNodeProperty(node.ttl, node.ttlType, node, msg);
             var units = RED.util.evaluateNodeProperty(node.units, node.unitsType, node, msg);
+            
+            // Get the property value to use as the ID.
+            var key = RED.util.evaluateNodeProperty(node.key, node.keyType, node, msg);
+            var id = (msg.hasOwnProperty(key)) ? msg[key] : msg._msgid;
+            if(id.indexOf("/") > -1 || id.indexOf("\\") > -1) {
+                node.error('The value of the defined ID Property may not contain a "/" or "\\" character.');
+                return done();
+            }
 
-            if (isNaN(ttl))
+            // ttl is required.
+            if (isNaN(ttl)) {
                 done("TTL must be a number.");
+            }
 
+            // convert ttl to milliseconds.
             switch (String(units).toLowerCase()) {
                 case "milliseconds":
                     break;
@@ -144,39 +173,49 @@ module.exports = function (RED) {
             }
 
             // If the msg already has a timer it's a repeat.
+            // send it along and go no further.
             if(node.timers[id]){
-                this.send([msg, null, null]);
+                send([msg, null, null]);
                 return done();
             }
 
-            // If the msg was sourced from the boot cache.
+            // If the msg came from the persistent store, send it on the third output.
             if (msg._ack_cached_tod) {
-                this.send([null, null, msg]);
+                send([null, null, msg]);
                 ttl = msg._ack_cached_tod - Date.now();
             }
             else {
-                addCacheMsg(msg, Date.now() + ttl);
-                this.send([msg, null, null]);
+                addPersistentMsg(msg, Date.now() + ttl);
+                send([msg, null, null]);
             }
 
+            // If there is a ttl, schedule a timeout.
             if (ttl > 0) {
+                const timeout_msg = Object.assign({}, msg);
+                // function remove_timer() {
+                //     delete node.timers[id];
+                // };
                 node.timers[id] = {
-                    timer: timers.setTimeout((send, done) => {
-                        removeCacheMsg(id);
-                        send([null, msg, null]);
+                    timer: timers.setTimeout((send, done, ) => {
+                        removePersistentMsg(id);
+                        send([null, timeout_msg, null]);
                         done();
+                        delete node.timers[id];
                     }, Number(ttl), send, done),
                     done: done
                 }
             }
+
+            // If there is no ttl left, send it on the timeout output and remove from persistent store.
             else {
-                removeCacheMsg(id);
+                removePersistentMsg(id);
                 send([null, msg, null]);
                 done();
             }
         });
 
-        this.on("close", function () {
+        // On close remove all timers and link listeners.
+        node.on("close", function () {
             Object.keys(node.timers).forEach((key) => {
                 timers.clearTimeout(node.timers[key].timer);
             });
@@ -185,11 +224,23 @@ module.exports = function (RED) {
                 RED.events.removeListener("node:" + link, handler);
             });
         });
+
+        // Read in all and send all persisted messages.
+        if (node.persist) {
+            fs.readdirSync(NODE_FOLDER_NAME, { withFileTypes: true }).forEach(dirent => {
+                var raw = fs.readFileSync(path.join(NODE_FOLDER_NAME, dirent.name));
+
+                // This timeout is set to make sure that all other nodes have been initialized.
+                timers.setTimeout(() => {
+                    node.receive(JSON.parse(raw));
+                }, 100);
+            });
+        }
     });
 
     RED.nodes.registerType("ack-clear", function (n) {
         RED.nodes.createNode(this, n);
-        var event = "node:" + n.id;
+        const event = "node:" + n.id;
 
         this.on("input", function (msg) {
             msg._event = event;
